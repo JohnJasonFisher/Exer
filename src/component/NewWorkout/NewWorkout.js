079116321e628df626d7064641a336c7d9f199e0
JohnJasonFisher <jjfisher91@gmail.com>
2018-07-16 10:18:03 -0500

    Creates Button that starts and finishes a Workout

--- src/component/NewWorkout/NewWorkout.js
+++ src/component/NewWorkout/NewWorkout.js
@@ -3,8 +3,7 @@ import React from 'react'
 const NewWorkout = props => {
 	return (
 		<div className='NewWorkout'>
-			<button>Start a new Workout!</button>
-			<button>Finish a new Workout!</button>
+			<button onClick={props.click}>{props.buttonMsg}!</button>
 		</div>
 	)
 }
--- src/container/App.js
+++ src/container/App.js
@@ -3,11 +3,33 @@ import './App.css'
 import NewWorkout from '../component/NewWorkout/NewWorkout'
 
 class App extends Component {
+
+	constructor() {
+		super()
+		this.state = {
+			NewWorkout: false
+		}
+	}
+
+	startWorkoutHandler = () => {
+		let newState = {...this.state}
+		newState.NewWorkout = !this.state.NewWorkout
+		this.setState(newState)
+	}
+
 	render() {
+		let buttonMsgText = 'Start a new Workout!'
+		if (this.state.NewWorkout === true) {
+			buttonMsgText = 'Finish a new Workout'
+		}
+
 		return (
-		<div className="App">
-			<NewWorkout/>
-		</div>
+			<div className="App">
+				<NewWorkout
+					buttonMsg={buttonMsgText}
+					click={this.startWorkoutHandler}
+				/>
+			</div>
 		)
 	}
 }
