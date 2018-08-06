d43c587ed703d707982130134674d3af9cbcee36
JohnJasonFisher <jjfisher91@gmail.com>
2018-08-06 15:43:56 -0500

    Adds Collapsable Workouts

--- src/container/PastWorkouts/PastWorkouts.js
+++ src/container/PastWorkouts/PastWorkouts.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import Axios from '../../../node_modules/axios'
-import Moment from 'moment'
 
 import ExistingExercises from '../../component/ExistingExercises/ExistingExercises'
 import './PastWorkouts.css'
@@ -20,12 +19,9 @@ class ExistingWorkouts extends Component {
 	}
 
 	render() {
-		let listOfWorkouts = this.state.workouts.map((workout, index) => (
-			<React.Fragment key={'frag' + index}>
-				<h4 key={workout.date}>{Moment(workout.date).format('MMMM Do YYYY')}</h4>
-				<ExistingExercises key={index} exercises={Object.values(workout.exercises)}/>
-			</React.Fragment>
-		))
+		let listOfWorkouts = this.state.workouts.map((workout, index) =>
+			<ExistingExercises key={index} exercises={Object.values(workout.exercises)} date={workout.date}/>
+		)
 		return(
 			<div className='ExistingWorkouts'>
 				{listOfWorkouts}
