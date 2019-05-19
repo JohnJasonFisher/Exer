6742d76bd95e2e087efb5ed766584678ced18878
John Jason Fisher <jjfisher91@gmail.com>
2019-05-18 23:53:44 -0500

    adds-workout-content

--- src/components/FiveByFive/FiveByFive.js
+++ src/components/FiveByFive/FiveByFive.js
@@ -3,7 +3,39 @@ import React, {Component} from 'react'
 export default class FiveByFive extends Component {
     render() {
 		return(
-            <p>FiveByFive</p>
+            <p>
+                DAY 1 - Full Body
+
+                Back Squats 5 sets x 5 reps
+                Bent Over Rows 5 sets x 5 reps
+                Flat Barbell Press 5 sets x 5 reps
+                Push Press 5 sets x 5 reps
+                Cable Rope Triceps Extensions 3 sets x 10 reps
+                Lateral Raise 3 sets x 10 reps
+                Seated Calf Raise 3 sets x 10 reps
+
+
+                Day 2 - Full body
+
+                Deadlifts 5 sets x 5 reps
+                Pull-Ups 5 sets x 5 reps (weighted if needed)
+                Incline Press 5 sets x 5 reps
+                Overhead Press 5 sets x 5 reps
+                Barbell Curls 3 sets x 10 reps
+                Bent Over Reverse Flys 3 sets x 10 reps
+                Standing Calf Raises 3 sets x 10 reps
+
+
+                Day 3 - Full body
+
+                Front Squats 5 sets x 5 reps
+                Pendlay Rows 5 sets x 5 reps
+                Chest Dips 5 sets x 5 reps (weighted if needed)
+                Single Arm Dumbbell Shoulder Press 5 sets x 5 reps 
+                Single Arm Dumbbell Triceps Extensions 3 sets x 10 reps
+                Cable Front Raises 3 sets x 10 reps
+                Seated Calf Raises 3 sets x 10 reps
+            </p>
         )
     }
 }
